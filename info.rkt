1f0510d8e50ca3d22b3ba7ee65cce117450d44a0
John Clements <clements@racket-lang.org>
2014-08-26 09:41:38 -0700

    updated dependencies

--- info.rkt
+++ info.rkt
@@ -1,3 +1,10 @@
 #lang setup/infotab
 (define collection 'multi)
 
+(define deps 
+  (list "base"
+        "compatibility-lib"
+        "scheme-lib"))
+(define build-deps
+  (list "rackunit-lib"
+        "srfi-lite-lib"))
